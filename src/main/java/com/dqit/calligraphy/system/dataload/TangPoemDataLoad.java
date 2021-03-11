4ec43461505edfdaca04ff47be113bc8b8106aad
duanqiangk <3122013373@qq.com>
2021-03-11 15:51:31 +0800

    maven 依赖以及包名初始化

---
+++ src/main/java/com/dqit/calligraphy/system/dataload/TangPoemDataLoad.java
@@ -0,0 +1,9 @@
+package com.dqit.calligraphy.system.dataload;
+
+/**
+ * @Author qiang.duan
+ * @Date 2021/3/11 15:50
+ * @Description
+ */
+public class TangPoemDataLoad {
+}
---
+++ src/main/java/com/dqit/calligraphy/util/JsonUtil.java
@@ -0,0 +1,9 @@
+package com.dqit.calligraphy.util;
+
+/**
+ * @Author qiang.duan
+ * @Date 2021/3/11 15:50
+ * @Description
+ */
+public class JsonUtil {
+}
