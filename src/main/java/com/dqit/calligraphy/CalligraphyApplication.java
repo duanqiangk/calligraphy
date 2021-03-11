baa6eb8e1ad67d54add1d925f13ef93bcb19a9c6
duanqiangk <3122013373@qq.com>
2021-03-11 14:14:09 +0800

    项目初始化

---
+++ src/main/java/com/dqit/calligraphy/CalligraphyApplication.java
@@ -0,0 +1,13 @@
+package com.dqit.calligraphy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalligraphyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalligraphyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dqit/calligraphy/CalligraphyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dqit.calligraphy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalligraphyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
